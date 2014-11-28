4fe0dc5588f497a9e5c1e26903881f82d6933e4c
Greg Bergé <neoziro@gmail.com>
2014-11-28 14:24:40 +0100

    Add pull method.

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect;
 var sqs = require('./index');
+var Promise = require('promise');
 
 describe('ya-sqs', function () {
   describe('#createQueue', function () {
@@ -35,7 +36,52 @@ describe('ya-sqs', function () {
     });
 
     it('should push a new message in the queue', function (done) {
-      queue.push('message', done);
+      queue.push('test message', done);
+    });
+
+    it('should push a new message in the queue (promise)', function () {
+      return queue.push('test message');
+    });
+  });
+
+  describe('#queue.pull', function () {
+    var queue;
+
+    beforeEach(function () {
+      queue = sqs.createQueue({
+        aws: {
+          region: process.env.AWS_REGION,
+          accessKeyId: process.env.AWS_ACCESS_KEY,
+          secretAccessKey: process.env.AWS_SECRET
+        },
+        queueUrl: process.env.QUEUE_URL
+      });
+    });
+
+    beforeEach(function () {
+      return queue.push('test message');
+    });
+
+    it('should pull message', function (done) {
+      queue.once('message processed', function () {
+        done();
+      });
+
+      queue.pull(function (message, next) {
+        expect(message).to.equal('test message');
+        next();
+      });
+    });
+
+    it('should pull message (promise)', function (done) {
+      queue.once('message processed', function () {
+        done();
+      });
+
+      queue.pull(function (message) {
+        expect(message).to.equal('test message');
+        return Promise.resolve();
+      });
     });
   });
 });
